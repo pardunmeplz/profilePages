856d4221ea517e1fe69475ab46cd27ae578bbbb0
PDundh <dundh.parth@gmail.com>
2022-08-25 19:48:29 +0530

    wrong file uploaded

--- src/Four.tsx
+++ src/Four.tsx
@@ -3,11 +3,5 @@ import { View, Text } from "react-native"
 import styles from "./FourStyle"
 
 export default function Four() {
-    return <View style={styles.container}>
-        <View style={{ flex: 0.4, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={styles.title}>Profile</Text>
-            <View style={styles.picture} />
-        </View>
-        <View style={styles.tab}></View>
-    </View>
+    return <View></View>
 }
